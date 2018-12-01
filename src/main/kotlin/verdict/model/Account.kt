be1ebfe238055c867ae33d65072c98f4e9d9ab42
IbukunF <irf23@drexel.edu>
2018-12-01 14:33:51 -0500

    Fix build dependencies

--- src/main/kotlin/verdict/model/Account.kt
+++ src/main/kotlin/verdict/model/Account.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.hibernate.annotations.NaturalId
 import javax.persistence.*
+
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
@@ -12,17 +13,21 @@ import javax.validation.constraints.Size
 
 data class Account(@Column(name = "email", nullable = false)
                    var email: String,
+
                    @NaturalId
                    @Column(name = "username", unique = true, nullable = false)
                    @NotNull
                    @Size(min = 3, max = 255)
                    var username: String = email,
+
                    @Size(min = 8)
                    @get:JsonIgnore
                    @set:JsonProperty
                    var password: String? = null,
+
                    var firstName: String,
-                   var lastName: String) {
+                   var lastName: String)
+{
 
     @Id
     @GeneratedValue
