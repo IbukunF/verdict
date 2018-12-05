01d0e2fe71b05c88778c9ebdb399ee346d619ef1
IbukunF <irf23@drexel.edu>
2018-12-05 13:33:07 -0500

    Rename home controller

--- src/main/kotlin/verdict/kotlin/Standard.kt
+++
@@ -1,9 +0,0 @@
-package verdict.kotlin
-
-/**
-
- * Calls the specified function [block] with the given [receiver] as its receiver and returns Unit.
-
- */
-
-//inline fun <T> configure(receiver: T, block: T.() -> Unit) = receiver.block()
